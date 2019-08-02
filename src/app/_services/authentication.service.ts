9f67361654ed596bea987d5136f0d59448f680a3
Marcos P. Amadio <mpamadio@hotmail.com>
2019-08-02 07:37:42 -0300

    alteraçoes login

--- src/app/_services/authentication.service.ts
+++ src/app/_services/authentication.service.ts
@@ -27,6 +27,7 @@ export class AuthenticationService {
       // store user details and jwt token in local storage to keep user logged in between page refreshes
       localStorage.setItem('currentUser', JSON.stringify(user));
       this.currentUserSubject.next(user);
+      //teste
 
       return user
     }))
